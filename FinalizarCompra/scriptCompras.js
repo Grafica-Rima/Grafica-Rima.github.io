15edc52349d701ade616c8ec35ead70c20ea3cd7
Grafica-Rima <agustinverteramo@gmail.com>
2024-04-29 12:10:04 -0300

    Initial Commit

---
+++ FinalizarCompra/scriptCompras.js
@@ -0,0 +1,445 @@
+const radioEnvioDomicilio = document.getElementById('envioDomicilio');
+const radioPuntoEntrega = document.getElementById('puntoEntrega');
+const campoDomicilio = document.getElementById('puntoEntregaDomicilio');
+const inputDomicilio = document.getElementById('domicilioPuntoEntrega');
+const botonGuardar = document.getElementById('guardarDomicilio');
+
+let ultimaSeleccion = null;
+let metodoEnvioSeleccionado = false;
+
+radioEnvioDomicilio.addEventListener('change', function() {
+  if (this.checked) {
+    ultimaSeleccion = 'envioDomicilio';
+    campoDomicilio.style.display = 'none'; // Asegúrate de ocultar el campo de domicilio si cambia a "Envío a domicilio"
+    metodoEnvioSeleccionado = true;
+  }
+});
+
+radioPuntoEntrega.addEventListener('change', function() {
+  if (this.checked) {
+    ultimaSeleccion = 'puntoEntrega';
+    campoDomicilio.style.display = 'block';
+    metodoEnvioSeleccionado = true;
+  }
+});
+
+botonGuardar.addEventListener('click', function() {
+  // Si no se ha seleccionado ninguna opción, muestra un mensaje y retorna false
+  if (ultimaSeleccion === null) {
+    alert('Por favor, selecciona una opción antes de guardar.');
+    return false;
+  }
+
+  // Obtener el valor del campo de domicilio
+  const domicilioValue = inputDomicilio.value.trim();
+
+  // Validar que se haya ingresado un domicilio si la última opción fue "Punto de entrega"
+  if (ultimaSeleccion === 'puntoEntrega' && domicilioValue === '') {
+    alert('Por favor, ingresa un domicilio válido.');
+    return false;
+  }
+
+  const formatoDomicilio = /^[A-Za-z\s]+ \d+, [A-Za-z\s]+$/;
+  if (!formatoDomicilio.test(domicilioValue)) {
+    alert('El formato del domicilio no es válido. Ejemplo válido: "Nombre de calle" "123" "," "Localidad".');
+    return false;
+  }
+
+  // Puedes realizar otras validaciones aquí según tus necesidades
+
+  // Si se llega hasta aquí, el formulario es válido y puedes proceder con el guardado
+  alert('Domicilio guardado: ' + domicilioValue);
+  return true;
+
+  metodoEnvioSeleccionado = true;
+
+});
+
+
+// Manejar el evento beforeunload para mostrar la advertencia
+window.addEventListener('beforeunload', function(e) {
+  // Verificar si se ha seleccionado un método de envío
+  if (!metodoEnvioSeleccionado) {
+    const mensaje = 'Seleccione un método de envío antes de salir de la página. ¿Está seguro de que desea salir?';
+    e.returnValue = mensaje; // Estándar antiguo
+    return mensaje; // Estándar actual
+  }
+});
+
+function mostrarModal() {
+  let modal = document.getElementById("myModal");
+  modal.style.display = "block";
+}
+
+// Función para cerrar el modal
+function cerrarModal() {
+  let modal = document.getElementById("myModal");
+  modal.style.display = "none";
+}
+
+// Mostrar el modal cuando se hace clic en el enlace "Nosotros"
+document.querySelector('a[href="#nosotros"]').addEventListener('click', function(event) {
+  event.preventDefault();
+  mostrarModal();
+});
+
+function mostrarModalTrabaja() {
+  let modal = document.getElementById("modalTrabaja");
+  modal.style.display = "block";
+}
+
+// Función para cerrar el modal
+function cerrarModalTrabaja() {
+  let modal = document.getElementById("modalTrabaja");
+  modal.style.display = "none";
+}
+
+// Mostrar el modal cuando se hace clic en el enlace "Nosotros"
+document.querySelector('a[href="#trabaja-con-nosotros"]').addEventListener('click', function(event) {
+  event.preventDefault();
+  mostrarModalTrabaja();
+});
+
+function mostrarModalPrivacidad() {
+  let modal = document.getElementById("modalPrivacidad");
+  modal.style.display = "block";
+}
+
+// Función para cerrar el modal
+function cerrarModalPrivacidad() {
+  let modal = document.getElementById("modalPrivacidad");
+  modal.style.display = "none";
+}
+
+// Mostrar el modal cuando se hace clic en el enlace "Nosotros"
+document.querySelector('a[href="#privacidad"]').addEventListener('click', function(event) {
+  event.preventDefault();
+  mostrarModalPrivacidad();
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Detectamos si el dispositivo está en modo responsive
+  const isResponsive = window.matchMedia("(max-width: 768px)").matches;
+
+  // Si el dispositivo está en modo responsive, ocultamos el footer al cargar la página
+  if (isResponsive) {
+    document.querySelector("#footer").style.display = "none";
+  }
+
+  window.addEventListener("scroll", function () {
+    let scrollPosition = window.scrollY;
+    let windowHeight = window.innerHeight;
+    let documentHeight = document.body.offsetHeight;
+    let triggerPoint = documentHeight - windowHeight - 10;
+
+    // Mostramos el footer cuando se alcanza el punto de activación
+    if (scrollPosition > triggerPoint) {
+      document.querySelector("#footer").style.display = "block";
+    } else {
+      // Ocultamos el footer si no se ha alcanzado el punto de activación
+      if (isResponsive) {
+        document.querySelector("#footer").style.display = "none";
+      }
+    }
+  });
+});
+
+
+
+
+function validarNombre() {
+  let nombreInput = document.getElementById("nombre");
+  let nombreValue = nombreInput.value.trim().toUpperCase(); // Convertir a mayúsculas
+  let regexNombreTitular = /^[A-Z\s']+$/; // Modificar la expresión regular para aceptar solo mayúsculas
+
+  // Verificar si el campo tiene algún valor
+  if (nombreValue === "") {
+    alert("Ingrese su nombre completo");
+    return false;
+  }
+
+  // Verificar si hay al menos dos palabras
+  let palabras = nombreValue.split(/\s+/);
+  if (palabras.length < 2) {
+    alert("Ingrese correctamente el nombre");
+    nombreInput.value = "";
+    return false;
+  }
+
+  // Realizar la validación solo si hay datos
+  if (!regexNombreTitular.test(nombreValue)) {
+    alert("Ingrese un nombre válido");
+    nombreInput.value = "";
+    return false;
+  }
+
+  // Actualizar el valor del campo con el nombre convertido a mayúsculas
+  nombreInput.value = nombreValue;
+
+  return true;
+}
+
+
+
+function validarTarjeta() {
+  let tarjetaInput = document.getElementById("tarjeta");
+
+  // Verificar si el campo tiene algún valor
+  if (tarjetaInput.value.trim() !== "") {
+    // Elimina caracteres no numéricos y espacios
+    tarjetaInput.value = tarjetaInput.value.replace(/[^0-9]/g, '');
+
+    if (tarjetaInput.value.length !== 16) {
+      alert("Coloque los 16 dígitos de la tarjeta");
+      tarjetaInput.value = "";
+      return false;
+    } else {
+      // Inserta un espacio cada cuatro dígitos
+      tarjetaInput.value = tarjetaInput.value.match(/.{1,4}/g).join(' ');
+    }
+  }
+
+  return true;
+}
+
+function validarFecha() {
+  let fechaInput = document.getElementById("fecha");
+  let fecha = fechaInput.value.trim().replace(/\s/g, ''); // Eliminar espacios y trim
+
+  // Verificar si el campo tiene algún valor
+  if (fecha !== "") {
+    // Modificar la expresión regular para permitir la barra diagonal "/"
+    if (!/^[0-9]{2}\/[0-9]{2}$/.test(fecha)) {
+      alert("Coloque correctamente la fecha de vencimiento (MM/AA)");
+      fechaInput.value = "";
+      return false;
+    } else {
+      let mes = parseInt(fecha.slice(0, 2), 10);
+      let anio = parseInt(fecha.slice(3), 10);
+
+      if (
+        isNaN(mes) || mes < 1 || mes > 12 ||
+        isNaN(anio) || anio < 24 || anio > 30
+      ) {
+        alert("Coloque correctamente la fecha de vencimiento (MM/AA)");
+        fechaInput.value = "";
+        return false;
+      } else {
+        // No es necesario modificar la fecha en este caso
+      }
+    }
+  }
+
+  return true;
+}
+
+
+function validarCVV() {
+  let cvvInput = document.getElementById("cvv");
+
+  // Verificar si el campo tiene algún valor
+  if (cvvInput.value.trim() !== "") {
+    // Verificar si hay caracteres no numéricos
+    if (/[^0-9]/.test(cvvInput.value)) {
+      alert("Coloque correctamente el código de seguridad (solo se permiten números)");
+      cvvInput.value = "";
+      return false;
+    }
+
+    // Remover caracteres no numéricos
+    cvvInput.value = cvvInput.value.replace(/[^0-9]/g, '');
+
+    // Verificar si la longitud es diferente de 3
+    if (cvvInput.value.length !== 3) {
+      alert("Coloque correctamente el código de seguridad (deben ser 3 dígitos)");
+      cvvInput.value = "";
+      return false;
+    }
+  }
+
+  return true;
+}
+
+
+function validarDomicilio() {
+  // Obtener el valor del campo de domicilio
+  let domicilioInput = document.getElementById("domicilio");
+
+  // Verificar si el campo está vacío
+  if (domicilioInput.value.trim() === "") {
+    // No hacer nada si el campo está vacío
+    return true;
+  }
+
+  // Validar el formato del domicilio permitiendo coma (",")
+  const formatoDomicilio = /^[A-Za-z0-9\s]+ \d+, [A-Za-z\s]+$/;
+  
+  if (!formatoDomicilio.test(domicilioInput.value.trim())) {
+    // Mostrar alerta si el formato no es válido
+    alert('El formato del domicilio no es válido. Ejemplo válido: "Nombre de calle" "123" "," "Localidad".');
+    domicilioInput.value = "";
+    // Puedes agregar más acciones aquí según tus necesidades
+    return false;
+  }
+
+  // No se encontraron problemas, el domicilio es válido
+  return true;
+}
+
+  
+
+
+
+function validarTelefono() {
+  let telefonoInput = document.getElementById("telefono");
+  let telefonoValue = telefonoInput.value.trim();
+
+  // Verificar si el campo de teléfono no está vacío
+  if (telefonoValue !== "") {
+    // Verificar si la longitud del número de teléfono es 10 y los primeros dos números son "11"
+    if (telefonoValue.length === 10 && telefonoValue.startsWith("11")) {
+      // El número de teléfono es válido
+    } else {
+      alert("Coloque correctamente su número de teléfono");
+      telefonoInput.value = "";
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function validarEmail() {
+  let emailInput = document.getElementById("email");
+
+  // Verificar si se ingresó algún dato
+  if (emailInput.value.trim() !== "") {
+    let emailRegex = /^[^\s@]+@(gmail\.com|hotmail\.com|outlook\.com)$/;
+
+    // Verificar si el correo cumple con el formato y el dominio
+    if (!emailRegex.test(emailInput.value)) {
+      alert("Ingrese un correo electrónico válido por favor");
+      emailInput.value = "";
+      return false;
+    }
+  }
+
+  return true;
+}
+
+
+// Función para validar el formulario y redireccionar si es válido
+function validarYRedireccionar() {
+  if (validarFormulario()) {
+    redireccionarAFinalizada();
+  }
+}
+
+// Función para validar el formulario
+function validarFormulario() {
+  // Obtener los valores de los campos del formulario
+  const nombreValue = document.getElementById("nombre").value.trim();
+  const tarjetaValue = document.getElementById("tarjeta").value.trim();
+  const fechaValue = document.getElementById("fecha").value.trim();
+  const cvvValue = document.getElementById("cvv").value.trim();
+  const domicilioValue = document.getElementById("domicilio").value.trim();
+  const telefonoValue = document.getElementById("telefono").value.trim();
+  const emailValue = document.getElementById("email").value.trim();
+
+  // Verificar si algún campo está vacío
+  if (nombreValue === "" || tarjetaValue === "" || fechaValue === "" || cvvValue === "" || domicilioValue === "" || telefonoValue === "" || emailValue === "") {
+    alert('Por favor, complete todos los campos del formulario.');
+    return false;
+  }
+
+  // Llamas a las funciones de validación individual
+  let nombreValido = validarNombre();
+  let tarjetaValida = validarTarjeta();
+  let fechaValida = validarFecha();
+  let cvvValido = validarCVV();
+  let domicilioValido = validarDomicilio();
+  let telefonoValido = validarTelefono();
+  let emailValido = validarEmail();
+  let metodoEnvioSeleccionado = false; // Variable para verificar si se ha seleccionado un método de envío
+
+  // Verificar si todos los campos del formulario están completos y son válidos
+  if (!nombreValido || !tarjetaValida || !fechaValida || !cvvValido || !domicilioValido || !telefonoValido || !emailValido) {
+    return false;
+  }
+
+  // Verificar si se ha seleccionado una opción de envío
+  if (radioEnvioDomicilio.checked || radioPuntoEntrega.checked) {
+    metodoEnvioSeleccionado = true;
+  }
+
+  // Si no se ha seleccionado un método de envío, mostrar una alerta
+  if (!metodoEnvioSeleccionado) {
+    alert('Seleccione un método de envío.');
+    return false;
+  }
+
+  // Si el formulario está completo y se ha seleccionado un método de envío, mostrar la alerta de compra finalizada
+  alert('Compra finalizada. Le llegará por correo los detalles de la compra');
+  document.getElementById('botonPagar').style.display = 'none'; // Oculta el botón de pago
+  document.getElementById('tuFormularioId').submit(); // Envía el formulario
+  return true;
+}
+
+// Función para redireccionar a la página finalizada
+function redireccionarAFinalizada() {
+  // Redirigir a la página finalizada.html
+  window.location.href = "http://127.0.0.1:5500/FinalizarCompra/Pagfin/finalizada.html";
+}
+
+// Las funciones de validación individuales y otros códigos relacionados deben ir aquí
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Recuperar los detalles de la compra del almacenamiento local
+  const carritoGuardado = localStorage.getItem('carrito');
+  if (carritoGuardado) {
+      const carrito = JSON.parse(carritoGuardado);
+
+      // Mostrar los detalles de la compra en la página
+      mostrarDetallesCompra(carrito);
+  }
+});
+
+function mostrarDetallesCompra(carrito) {
+  const detallesCompraElemento = document.getElementById('detallesCompra');
+
+  const tabla = document.createElement('table');
+  tabla.innerHTML = `
+      <tr>
+          <th>Producto</th>
+          <th>Precio</th>
+          <th>Cantidad</th>
+          <th>Total</th>
+      </tr>
+  `;
+
+  let totalCompra = 0;
+
+  carrito.forEach(item => {
+      const fila = document.createElement('tr');
+      fila.innerHTML = `
+          <td>${item.nombre}</td>
+          <td>${item.precio}</td>
+          <td>${item.cantidad}</td>
+          <td>${(parseFloat(item.precio) * item.cantidad).toFixed(2)}</td>
+      `;
+      tabla.appendChild(fila);
+
+      totalCompra += parseFloat(item.precio) * item.cantidad;
+  });
+
+  const filaTotal = document.createElement('tr');
+  filaTotal.innerHTML = `
+      <td colspan="3"><strong>Total:</strong></td>
+      <td><strong>${totalCompra.toFixed(2)}</strong></td>
+  `;
+  tabla.appendChild(filaTotal);
+
+  detallesCompraElemento.appendChild(tabla);
+}
---
+++ script.js
@@ -0,0 +1,244 @@
+let carrito = [];
+
+function añadirAlCarrito(nombre, precio) {
+  const cantidadSeleccionada = document.querySelector('.producto[data-nombre="' + nombre + '"] .cantidad select').value;
+
+  const productoExistente = carrito.find(producto => producto.nombre === nombre);
+
+  if (productoExistente) {
+    productoExistente.cantidad += parseInt(cantidadSeleccionada);
+  } else {
+    carrito.push({ nombre, precio, cantidad: parseInt(cantidadSeleccionada) });
+  }
+
+  actualizarContadorCarrito();
+  actualizarListaCarrito();
+
+  localStorage.setItem('carrito', JSON.stringify(carrito));
+}
+
+
+
+function actualizarContadorCarrito() {
+  const contadorCarrito = document.getElementById('contadorCarrito');
+  contadorCarrito.textContent = carrito.reduce((total, producto) => total + (producto.cantidad || 1), 0);
+}
+
+function mostrarDetalles(nombre, precio) {
+  console.log(`Detalles de ${nombre}. Precio: $${precio}`);
+}
+
+function actualizarListaCarrito() {
+  const listaCarritoSidebar = document.getElementById('listaCarritoSidebar');
+  const totalCarritoSidebar = document.getElementById('totalCarritoSidebar');
+
+  listaCarritoSidebar.innerHTML = '';
+
+  let totalCarrito = 0;
+  carrito.forEach(producto => {
+    const listItem = document.createElement('li');
+    const iconoBasura = document.createElement('img');
+    iconoBasura.src = 'http://127.0.0.1:5500/Imagenes/basura.jpg';
+    iconoBasura.alt = 'Eliminar';
+    iconoBasura.classList.add('eliminar-producto');
+
+    iconoBasura.dataset.nombre = producto.nombre;
+
+    iconoBasura.addEventListener('click', function() {
+      const nombreProducto = this.dataset.nombre;
+      eliminarProducto(nombreProducto);
+    });
+
+    listItem.appendChild(iconoBasura);
+
+    const contenidoProducto = `${producto.nombre} x${producto.cantidad} - $${producto.precio * (producto.cantidad || 1)}`;
+    listItem.appendChild(document.createTextNode(contenidoProducto));
+
+    listaCarritoSidebar.appendChild(listItem);
+
+    totalCarrito += producto.precio * (producto.cantidad || 1);
+  });
+
+  totalCarritoSidebar.textContent = `$${totalCarrito.toFixed(2)}`;
+}
+
+function eliminarProducto(nombre) {
+  carrito = carrito.filter(producto => producto.nombre !== nombre);
+  actualizarContadorCarrito();
+  actualizarListaCarrito();
+}
+
+function agregarEventoEliminarProducto() {
+  const iconosBasura = document.querySelectorAll('.eliminar-producto');
+  iconosBasura.forEach(icono => {
+    icono.addEventListener('click', function() {
+      const nombreProducto = this.dataset.nombre;
+      eliminarProducto(nombreProducto);
+    });
+  });
+}
+
+function mostrarCarrito() {
+  document.getElementById("sidebar").style.display = "block";
+}
+
+function ocultarCarrito() {
+  document.getElementById("sidebar").style.display = "none";
+}
+
+function subirArchivo() {
+  const inputArchivo = document.getElementById('#archivoInput');
+
+  if (archivoInput.files.length === 0) {
+    alert("Cargue un archivo para enviar.");
+  } else {
+    alert("Su archivo se ha cargado correctamente. Envíe un mensaje desde el formulario de contacto para ponernos en contacto.");
+  }
+}
+
+let cantidadAnterior = 0;
+
+function ajustarCantidad(nombre, cambio) {
+  const productoExistente = carrito.find(producto => producto.nombre === nombre);
+  if (productoExistente) {
+    productoExistente.cantidad = Math.max((productoExistente.cantidad || 0) + cambio, 0);
+    actualizarListaCarrito();
+    actualizarContadores();
+  }
+}
+
+function actualizarContadores() {
+  const productosConSelector = document.querySelectorAll('.producto .cantidad select');
+
+  productosConSelector.forEach(selector => {
+    const nombreProducto = selector.closest('.producto').dataset.nombre;
+    const productoExistente = carrito.find(producto => producto.nombre === nombreProducto);
+
+    if (productoExistente) {
+      productoExistente.cantidad = parseInt(selector.value);
+    }
+  });
+
+  actualizarListaCarrito();
+}
+
+const productosConSelector = document.querySelectorAll('.producto .cantidad select');
+
+productosConSelector.forEach(selector => {
+  selector.addEventListener('change', () => {
+    const nombreProducto = selector.closest('.producto').dataset.nombre;
+    ajustarCantidad(nombreProducto, selector.value - cantidadAnterior);
+    cantidadAnterior = selector.value;
+  });
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("scroll", function () {
+    let scrollPosition = window.scrollY;
+    let windowHeight = window.innerHeight;
+    let documentHeight = document.body.offsetHeight;
+    let triggerPoint = documentHeight - windowHeight - 10;
+
+    if (scrollPosition > triggerPoint) {
+      document.querySelector(".container-footer").style.display = "block";
+    } else {
+      document.querySelector(".container-footer").style.display = "none";
+    }
+  });
+});
+
+
+function mostrarModal() {
+  let modal = document.getElementById("myModal");
+  modal.style.display = "block";
+}
+
+function cerrarModal() {
+  let modal = document.getElementById("myModal");
+  modal.style.display = "none";
+}
+
+document.querySelector('a[href="#nosotros"]').addEventListener('click', function(event) {
+  event.preventDefault();
+  mostrarModal();
+});
+
+function mostrarModalTrabaja() {
+  let modal = document.getElementById("modalTrabaja");
+  modal.style.display = "block";
+}
+
+function cerrarModalTrabaja() {
+  let modal = document.getElementById("modalTrabaja");
+  modal.style.display = "none";
+}
+
+document.querySelector('a[href="#trabaja-con-nosotros"]').addEventListener('click', function(event) {
+  event.preventDefault();
+  mostrarModalTrabaja();
+});
+
+function mostrarModalPrivacidad() {
+  let modal = document.getElementById("modalPrivacidad");
+  modal.style.display = "block";
+}
+
+function cerrarModalPrivacidad() {
+  let modal = document.getElementById("modalPrivacidad");
+  modal.style.display = "none";
+}
+
+document.querySelector('a[href="#privacidad"]').addEventListener('click', function(event) {
+  event.preventDefault();
+  mostrarModalPrivacidad();
+});
+
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Enviando...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_ah8n5jh';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Enviar correo';
+      alert('Correo enviado');
+    }, (err) => {
+      btn.value = 'Enviar correo';
+      alert(JSON.stringify(err));
+    });
+});
+
+function inicializarBusqueda() {
+  const input = document.getElementById('searchInput');
+  input.addEventListener('input', buscar);
+}
+
+function buscar() {
+  const input = document.getElementById('searchInput');
+  const searchTerm = input.value.trim().toLowerCase(); // Obtener el término de búsqueda en minúsculas, eliminando espacios en blanco al inicio y al final
+
+  const productos = document.querySelectorAll('.producto');
+  productos.forEach(producto => {
+    const nombre = producto.dataset.nombre.toLowerCase(); // Obtener el nombre del producto en minúsculas
+    if (nombre.includes(searchTerm)) {
+      producto.style.display = 'block'; // Mostrar el producto si coincide con el término de búsqueda
+    } else {
+      producto.style.display = 'none'; // Ocultar el producto si no coincide con el término de búsqueda
+    }
+  });
+
+  // También puedes ocultar los encabezados h2 u otros elementos aquí
+  const encabezados = document.querySelectorAll('h2'); // Ajusta el selector según los elementos que desees ocultar/mostrar
+  encabezados.forEach(encabezado => {
+    encabezado.style.display = searchTerm ? 'none' : 'block'; // Si hay un término de búsqueda, oculta los encabezados, de lo contrario, muéstralos
+  });
+}
+
+document.addEventListener('DOMContentLoaded', inicializarBusqueda);
